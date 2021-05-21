821e77b6c2ed4e51335b93cbc19a7a78d5b0cb82
xgschwi <xaviergschwind@gmail.com>
2021-05-21 13:23:29 -0400

    Frontend pages established

--- full-stack1/src/App.js
+++ full-stack1/src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+import {Home} from './Home';
+import {Department} from './Department';
+import {Employee} from './Employee';
+import {Navigation} from './Navigation';
+
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div className="container">
+      <h3 className ="m-3 d-flex justify-content-center">
+        React JS Tutorial
+      </h3>
+
+      <Navigation/>
+
+      <Switch>
+        <Route path='/' component={Home} exact/>
+        <Route path='/department' component={Department}/>
+        <Route path='/employee' component={Employee}/>
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
